de354a424330bdd79ed8b8591f6817b1474b9b5d
eitegbe <eitegbe@gmail.com>
2015-05-08 00:11:52 -0700

    Fixed

--- src/main/java/edu/sjsu/cmpe273/facebookarchiver/HomeController.java
+++ src/main/java/edu/sjsu/cmpe273/facebookarchiver/HomeController.java
@@ -4,17 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Version;
+import com.restfb.types.User;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.FacebookApi;
 import org.scribe.exceptions.OAuthException;
 import org.scribe.model.Token;
 import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
@@ -31,7 +31,7 @@ public class HomeController {
     private static final String STATE = "state";
     private String client_id = "966288596744764";
     private String app_secret = "dea83246d01afcb06464a13347a10064";
-    private String url = "http://localhost:8080/";
+    private String url = "http://localhost:8080";
     private ObjectMapper objectMapper;
     private OAuthService oAuthService;
     private FacebookClient facebookClient;
@@ -58,29 +58,34 @@ public class HomeController {
     public RedirectView startAuthentication(HttpSession httpSession) throws OAuthException {
         String state = UUID.randomUUID().toString();
         httpSession.setAttribute(STATE, state);
-        httpSession.removeAttribute(STATE);
-        String authorizationUrl = oAuthService.getAuthorizationUrl(Token.empty())
-                +"&"+STATE+"="+state;
+        String authorizationUrl = oAuthService.getAuthorizationUrl(Token.empty())+"&"+STATE+"="+state;
         return new RedirectView(authorizationUrl);
     }
     //method handles the callback from facebook
-    @RequestMapping(value="/auth/facebook/callback", method = RequestMethod.GET)
-    public RedirectView callback(@RequestParam("code")String code, @RequestParam(STATE)String state, HttpSession httpsession) throws IOException
+    @RequestMapping(value="/auth/facebook/callback")
+    public String callback(@RequestParam("code")String code, @RequestParam(STATE)String state, HttpSession httpsession) throws IOException
     {
         //checks state parameter.
         String stateFromSession = (String)httpsession.getAttribute(STATE);
         httpsession.removeAttribute(STATE);
         if(!state.equals(stateFromSession)) { //incase of failure redirects user to login
-            return new RedirectView("login");
+            return "login";
         }
         //Exchanges the access token
         Token accessToken = getAccessToken(code);
         this.facebookClient = new DefaultFacebookClient(accessToken.getToken(), Version.VERSION_2_2);
-        return new RedirectView("logged"); //successfully logged in.
+        return "logged"; //successfully logged in.
     }
 
     private Token getAccessToken(String code) {
         Verifier verify = new Verifier(code);
         return oAuthService.getAccessToken(Token.empty(), verify);//Token.Empty() method in scribe and handles OAuthservice for both OAuth1 and 2.
     }
+
+    @RequestMapping(value="/{user-id}/attending", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody void getName(@PathVariable("user-id")String userId) {
+        User me = facebookClient.fetchObject("me", User.class);
+    }
+
 }
--- src/main/java/edu/sjsu/cmpe273/facebookarchiver/entity/VideoAttributes.java
+++
@@ -1,7 +0,0 @@
-package edu.sjsu.cmpe273.facebookarchiver.entity;
-
-/**
- * Created by emy on 5/1/15.
- */
-public class VideoAttributes {
-}
